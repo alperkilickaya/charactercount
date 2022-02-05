dfd183a85cf377312ab90e1f3dfa06cf61d22322
alperkilickaya <48261006+alperkilickaya@users.noreply.github.com>
2022-02-05 19:02:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,32 @@
-
-
-var textarea = document.querySelector('textarea');
-var current = document.getElementById('current');
-var regexp = /[ğüşıİöç]/gi;
-
+let textarea = document.querySelector("textarea");
+let current = document.getElementById("current");
+let regexp = /[ğüşıİöç]/gi;
+let ikazz = document.getElementById("alper");
 
 function sayac() {
+  let yazi = textarea.value;
+  let boslukalinmis = yazi.replace(/ +/g, "");
+  let turkcetekrarsayisi = (yazi.match(regexp) || []).length;
+  let uzunluk = boslukalinmis.length;
+  window.toplam = uzunluk + turkcetekrarsayisi;
 
-    var yazi = textarea.value;
-    var boslukalinmis = yazi.replace(/ +/g, "")
-    var turkcetekrarsayisi =  (yazi.match(regexp) || []).length;
-    var uzunluk = boslukalinmis.length;
-    var toplam = uzunluk + turkcetekrarsayisi;
+  current.innerText = toplam;
 
-    current.innerText = toplam;
-    
-    var ikazz = document.getElementById("alper");
-
-    if(toplam <= 155){
-        ikazz.classList.add("ikaz");
-        //textarea.maxLength = 156;
-    }else{
-        ikazz.classList.remove("ikaz");
-    }
+  if (toplam <= 155) {
+    ikazz.classList.add("ikaz");
+    //textarea.maxLength = 156;
+  } else {
+    ikazz.classList.remove("ikaz");
+  }
 }
 
-function gonder(){
-    var yazi = textarea.value;
-    var boslukalinmis = yazi.replace(/ +/g, "")
-    var turkcetekrarsayisi =  (yazi.match(regexp) || []).length;
-    var uzunluk = boslukalinmis.length;
-    var toplam = uzunluk + turkcetekrarsayisi;
-    var mesaj_sayi = Math.ceil(toplam/155);
-    
-        Swal.fire({
-            title: 'Uyarı!',
-            html: 'Toplam ' + '<b>' + mesaj_sayi + ' MESAJ' + '</b>' + ' gönderilecek?',
-            icon: 'warning',
-            confirmButtonText: 'Tamam'
-        });
+function gonder() {
+  let mesaj_sayi = Math.ceil(toplam / 155);
+
+  Swal.fire({
+    title: "Uyarı!",
+    html: "Toplam " + "<b>" + mesaj_sayi + " MESAJ" + "</b>" + " gönderilecek?",
+    icon: "warning",
+    confirmButtonText: "Tamam",
+  });
 }
